764664875fa468fd3687d12409593b5e3d59ee7b
WilsonMariano <mgw009@gmail.com>
2018-07-31 13:16:00 -0300

    Cambios para el localhost

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   cantPagPrincipal: 15,
   cantPagListados: 10,
-  rutaApi: 'http://l9000590.ferozo.com/apiInternos/'
+ // rutaApi: 'http://l9000590.ferozo.com/apiInternos/'
+ rutaApi: 'http://localhost:8080/internos/apiInternos/index.php/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ export const environment = {
   production: false,
   cantPagPrincipal: 15,
   cantPagListados: 10,
-  rutaApi: 'http://l9000590.ferozo.com/apiInternos/'
+  // rutaApi: 'http://l9000590.ferozo.com/apiInternos/'
+  rutaApi: 'http://localhost:8080/internos/apiInternos/index.php/'
 };
 
 /*
