6b0ce4635e1476b8c72993d1c9a1b55c425609ee
WilsonMariano <mgw009@gmail.com>
2018-08-03 10:15:20 -0300

    Cambios esteticos

--- src/app/components/nabvar/nabvar.component.ts
+++ src/app/components/nabvar/nabvar.component.ts
@@ -14,7 +14,7 @@ export class NabvarComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private authService: AuthService) { }
+    public authService: AuthService) { }
 
   ngOnInit() {
   }
