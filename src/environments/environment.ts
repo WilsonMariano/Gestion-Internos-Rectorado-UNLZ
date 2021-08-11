b18a9bb1dcfcdbe6afdebe9bf647a1990cbe9191
Mariano Wilson <mariano.wilson@bp-4.com>
2021-08-11 14:29:20 -0300

    Habilito botón de login - Cambio url api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   cantPagListados: 10,
   // rutaApi: 'http://l9000590.ferozo.com/apiInternos/'
-  rutaApi: 'http://172.16.4.26/internos/apiInternos/index.php/'
+  rutaApi: 'http://172.16.4.34/internos/apiInternos/index.php/'
   // rutaApi: 'http://localhost:8080/internos/apiInternos/index.php/'
 };
 
